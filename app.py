ce75556ffd73cb3a5c58da466d85e67e74af8092
ashish <agashish1998@gmail.com>
2020-06-17 18:03:45 +0530

    implemented oop

--- app.py
+++ app.py
@@ -1,120 +1,75 @@
 #python web app for bollywood game
 from flask import Flask, render_template, request, redirect
-from movie_list import movies
-import random
+import bollywood_oop as b
 
-player_name = ""
-wrong_guessed_letters = ['A', 'E', 'I', 'O', 'U']
-guessed_letters = wrong_guessed_letters
-trials = 9
-pre_movie = ""
-player_input = ""
-
-def movie_selector():
-    word_index = random.randint(0, len(movies)-1)
-    return movies[word_index].upper()
-
-global movie_name
-movie_name = movie_selector().upper()
-
-def check_guess(x):
-    for a in movie_name:
-        if(a == x):
-            return True
-    return False  
-
-def print_movie():
-    s = ""
-    for a in movie_name:
-        if(a == ' '):
-            s += " "
-        elif(a in guessed_letters):
-            s += a
-        else:
-            s += '_'
-    return s.split()
-
-def check_final_solution():
-    for a in movie_name:
-        if (a not in guessed_letters and a != ' '):
-            return False
-
-    return True
-
-def print_bollywood():
-    s = "BOLLYWOOD"
-    x = ""
-    for i in range(trials):
-        x += s[i]
-    for i in range(trials, 9):
-        x += '_'
-    return x
-
-def reseter(arr):
-    while(len(arr) > 5):
-        arr.pop()
+game = b.bollywood()
 
 app = Flask(__name__)
-@app.route("/")
 
+@app.route("/")
 def start():
-    return render_template("start.html")
+	return render_template("start.html")
 
 @app.route("/play", methods=["POST"])
 def play():
-    #print(player_name)
-    global player_name
-    temp1 = request.form.get("name")
-    player_input = request.form.get("user")
-    if(temp1 != None):
-        player_name = temp1.upper()
+	temp1 = request.form.get("name")
+	player_input = request.form.get("user")
+	if(temp1 != None):
+		game.player_name = temp1.upper()
 
-    if player_input != None:
-        global trials
-        global guessed_letters
-        global wrong_guessed_letters
-        global movie_name
-        global pre_movie
-        player_input = player_input.upper()
+	if player_input != None:
+		game.input_handle(player_input)
+		
 
-        for a in player_input:
-            if(a < 'A' or a >'Z' or (a in guessed_letters) or (a in wrong_guessed_letters)):
-                continue
-            if check_guess(a):
-                guessed_letters.append(a)
-            else:
-                trials = trials -1
-                wrong_guessed_letters.append(a)
+		if game.trials < 1:
+	
+			game.reset()
+			return redirect("/lost")
 
-        if trials < 1:
-            reseter(guessed_letters)
-            reseter(wrong_guessed_letters)
-            trials = 9
-            pre_movie = movie_name
-            movie_name = movie_selector().upper()
-            return redirect("/lost")
+		if(game.check_final_solution()):
 
-        if(check_final_solution()):
-            reseter(guessed_letters)
-            reseter(wrong_guessed_letters)
-            trials = 9
-            pre_movie = movie_name
-            movie_name = movie_selector().upper()
-            return redirect("/win")
+			game.reset()
+			return redirect("/win")
             
-    return render_template("game.html", bollywood_string=print_bollywood(), movie_name=print_movie())
+	return render_template("game.html", bollywood_string=game.print_bollywood(), movie_name=game.print_movie())
 
 @app.route("/win")
 def win():
-    return render_template("win.html", player_name=player_name, movie_name=pre_movie)
+	return render_template("win.html", player_name=game.player_name, movie_name=game.pre_movie)
 
 @app.route("/lost")
 def lost():
-    return render_template("lost.html", player_name=player_name, movie_name=pre_movie)
+	return render_template("lost.html", player_name=game.player_name, movie_name=game.pre_movie)
 
 @app.route("/rules")
 def rules():
-    return render_template("rules.html")
+	return render_template("rules.html")
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ bollywood_oop.py
@@ -0,0 +1,92 @@
+
+from movie_list import movies
+import random
+
+max_trials = 9
+starter = ['A', 'E', 'I', 'O', 'U']
+
+
+class bollywood:
+	
+	def __init__(self):
+		self.movie_name = random.choice(movies).upper()
+		self.player_name = ""
+		self.trials = 9
+		self.wrong_guessed_letters = starter
+		self.guessed_letters = starter
+		self.pre_movie = ""
+		
+	def check_guess(self, x):
+		for a in self.movie_name:
+			if a == x:
+				return True
+		return False
+		
+	def print_movie(self):
+		s = ""
+		print(self.guessed_letters)
+		for a in self.movie_name:
+			if a == ' ':
+				s += ' '
+			elif a in self.guessed_letters:
+				s += a
+			else:
+				s += '_'
+		return s.split()
+		
+	def check_final_solution(self):
+		for a in self.movie_name:
+			if a not in self.guessed_letters and a != ' ':
+				return False
+		return True
+		
+	def print_bollywood(self):
+		s = 'BOLLYWOOD'
+		x = ''
+		for i in range(self.trials):
+			x += s[i]
+		for i in range(self.trials, 9):
+			x += '_'
+		return x
+		
+	def reset(self):
+		print("resetting the game")
+		self.trials = max_trials
+		self.guessed_letters = ['A', 'E', 'I', 'O', 'U']
+		self.wrong_guessed_letters = ['A', 'E', 'I', 'O', 'U']	
+		self.pre_movie = self.movie_name
+		self.movie_name = random.choice(movies).upper()
+		print(self.guessed_letters, self.wrong_guessed_letters)
+		
+		
+	def input_handle(self, g):
+			
+		g = g.upper()
+		
+		for a in g:
+			if(a < 'A' or a >'Z' or (a in self.guessed_letters) or (a in self.wrong_guessed_letters)):
+				continue
+			if self.check_guess(a):
+				self.guessed_letters.append(a)
+			else:
+				self.trials = self.trials -1
+				self.wrong_guessed_letters.append(a)
+                
+                
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+	
